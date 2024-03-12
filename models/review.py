10e2075721d27eff37be07e5b8538f20006d3cb4
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-03-12 20:13:17 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -7,9 +7,9 @@ class Review(BaseModel):
     """Represent a review.
 
     Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
+        place_id (str): Place id.
+        user_id (str): User id.
+        text (str): text for the review.
     """
 
     place_id = ""
