2fa8d0576b23f111f02649d011ab341e6e8ba0a3
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2024-03-12 20:16:58 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,13 +4,13 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
+    """Represents a User.
 
     Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+        email (str): put email of the user.
+        password (str): password of the user.
+        first_name (str): first name of the user.
+        last_name (str): last name of the user.
     """
 
     email = ""
